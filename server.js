88c2b2881b6c93cec567d6ac45af1882ec09109b
Jasim Aboobacker <jasim.aboobacker@gmail.com>
2015-12-27 15:33:32 +0000

    Updated Mongoose version to 4

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ var mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost:27017/quotes');
 //mongoose.connect('mongodb://jasim:jasim@ds037095.mongolab.com:37095/helloworld');
 
-mongoose.connect('mongodb://jasim:jasim@iad1-mongos0.objectrocket.com:16084/qoutes');
+//mongoose.connect('mongodb://jasim:jasim@iad1-mongos0.objectrocket.com:16084/qoutes');
+
+mongoose.connect('mongodb://jasim:jasim@ds037095.mongolab.com:37095/heroku_ls58mq68');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT|| 8080;
+var port = process.env.PORT|| 8081;
 
 var router = express.Router();
 
